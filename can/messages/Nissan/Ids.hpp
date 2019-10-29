2c250ab0c3735086348b84df7c1a9293abbd71e7
Sam Nobs <3915450+samsta@users.noreply.github.com>
2019-10-29 17:37:16 +1300

    oops - forgot to add file

---
+++ can/messages/Nissan/Ids.hpp
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+
+#ifndef _CAN_MESSAGES_NISSAN_IDS_HPP
+#define _CAN_MESSAGES_NISSAN_IDS_HPP
+
+namespace can {
+namespace messages {
+namespace Nissan {
+
+enum Ids {
+   ID_LBC_DATA_REPLY = 0x7bb
+};
+
+enum LbcDataGroups {
+   GROUP_CELL_VOLTAGES = 2,
+   GROUP_PACK_TEMPERATURES = 4
+};
+
+}
+}
+}
+
+#endif
