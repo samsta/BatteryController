0fe88b9baf16b603752f0e4b1ad7b1b89bd8139f
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2021-09-13 12:18:06 +1200

    Fix broken previous push..
    See if I fixed broken git check.

--- util.hpp
+++ util.hpp
@@ -35,7 +35,6 @@ inline uint16_t limitScaledToSignedShort(float val, unsigned scale)
 
 inline uint8_t limitValueToByte(unsigned val)
 {
-   if (val < 0) return 0;
    if (val > 255) return 255;
    return val;
 }
