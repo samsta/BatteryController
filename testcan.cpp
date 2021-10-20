4a785a986c8e0f2b63a28a5748df9b1f72b17e2f
Sam Nobs <3915450+samsta@users.noreply.github.com>
2021-10-21 09:35:49 +1300

    trivial: Use only one definition of the colors

--- testcan.cpp
+++ testcan.cpp
@@ -42,19 +42,11 @@
 #include "core/LibGpiod/OutputPin.hpp"
 #include "core/Timer.hpp"
 #include "core/Callback.hpp"
+#include "logging/colors.hpp"
 
 using namespace core::libgpiod;
+namespace color = logging::color::ansi;
 
-namespace color {
-const char* red   = "\x1b[31m";
-const char* bright_red   = "\x1b[91m";
-const char* green = "\x1b[32m";
-const char* bright_green = "\x1b[92m";
-const char* blue  = "\x1b[34m";
-const char* bright_blue  = "\x1b[94m";
-const char* reset = "\x1b[0m";
-}
-              
 class NissanSink: public can::FrameSink
 {
 public:
