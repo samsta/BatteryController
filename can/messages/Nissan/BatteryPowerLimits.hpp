1b41d4e881710ea6d7e597e029929ccd189ffa7e
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2021-10-31 13:23:00 +1300

    testing

--- can/messages/Nissan/BatteryPowerLimits.hpp
+++ can/messages/Nissan/BatteryPowerLimits.hpp
@@ -2,7 +2,9 @@
 #ifndef CAN_MESSAGES_NISSAN_BATTERYPOWERLIMITS_HPP_
 #define CAN_MESSAGES_NISSAN_BATTERYPOWERLIMITS_HPP_
 
-#include "can/messages/Message.hpp"
+//#include "can/messages/Message.hpp"
+#include "can/StandardDataFrame.hpp"
+#include "can/messages/Nissan/Message.hpp"
 #include "logging/stream.hpp"
 
 namespace can {
@@ -15,8 +17,8 @@ namespace Nissan {
 class BatteryPowerLimits: public Message
 {
 public:
-   BatteryPowerLimits(const DataFrame& f);
    BatteryPowerLimits();
+   BatteryPowerLimits(const DataFrame& f);
 
    float getDischargePowerLimit() const;
    BatteryPowerLimits& setDischargePowerLimit(float power);
