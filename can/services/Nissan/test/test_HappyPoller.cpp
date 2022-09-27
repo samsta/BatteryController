8d6e54cf03b08c8a29faa1caaa22dffd9afbf2ae
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2022-09-27 23:34:33 +1300

    Update test_HappyPoller.cpp
    Changes needed to test as HappyPoller is set to 10 minutes and no messages are being sent at all.  Also 50c messages are no longer being sent.  Live testing has shown these messages are not necessary.

--- can/services/Nissan/test/test_HappyPoller.cpp
+++ can/services/Nissan/test/test_HappyPoller.cpp
@@ -24,7 +24,7 @@ TEST(HappyPoller, registersAndDeregistersPollingCallbackFor1000MillisecondPeriod
 
    {
       core::Invokable* invokable;
-      EXPECT_CALL(timer, registerPeriodicCallback(_, 200)).WillOnce(SaveArg<0>(&invokable));
+      EXPECT_CALL(timer, registerPeriodicCallback(_, 600000)).WillOnce(SaveArg<0>(&invokable));
       HappyPoller poller(sender, timer);
       EXPECT_CALL(timer, deregisterCallback(invokable));
    }
@@ -65,44 +65,44 @@ MATCHER_P(AsString, str, std::string(negation ? " doesn't equal " : " equals ") 
 
 TEST_F(HappyPollerTest, pollInvokesSender)
 {
-   EXPECT_CALL(sender, sink(_)).Times(3);
+   EXPECT_CALL(sender, sink(_)).Times(0);
 
    poll_callback->invoke();
 }
 
-TEST_F(HappyPollerTest, pollCyclesPollerMessages)
-{
-   for (int k : {1, 2})
-   {
-      SCOPED_TRACE(Message("k=") << k);
-
-      // starts at 3 because that the way it is in the car
-      EXPECT_CALL(sender, sink(AsString("11a#4E4004AA80000370")));
-      // starts at 0 just 'cus
-      EXPECT_CALL(sender, sink(AsString("1f2#106400B4001E0083")));
-      EXPECT_CALL(sender, sink(AsString("1d4#6E6E0000074621C0")));
-      poll_callback->invoke();
-
-      EXPECT_CALL(sender, sink(AsString("11a#4E4004AAC00000BD")));
-      EXPECT_CALL(sender, sink(AsString("1f2#106400B4001E0184")));
-      EXPECT_CALL(sender, sink(AsString("1d4#6E6E000047462107")));
-      poll_callback->invoke();
-
-      EXPECT_CALL(sender, sink(AsString("11a#4E400455000001AB")));
-      EXPECT_CALL(sender, sink(AsString("1f2#106400B4001E0285")));
-      EXPECT_CALL(sender, sink(AsString("1d4#6E6E0000874621CB")));
-      poll_callback->invoke();
-
-      EXPECT_CALL(sender, sink(AsString("11a#4E40045540000266")));
-      EXPECT_CALL(sender, sink(AsString("1f2#106400B4001E0386")));
-      EXPECT_CALL(sender, sink(AsString("1d4#6E6E0000C746210C")));
-      poll_callback->invoke();
-   }
-}
+//TEST_F(HappyPollerTest, pollCyclesPollerMessages)
+//{
+//   for (int k : {1, 2})
+//   {
+//      SCOPED_TRACE(Message("k=") << k);
+//
+//      // starts at 3 because that the way it is in the car
+//      EXPECT_CALL(sender, sink(AsString("11a#4E4004AA80000370")));
+//      // starts at 0 just 'cus
+//      EXPECT_CALL(sender, sink(AsString("1f2#106400B4001E0083")));
+//      EXPECT_CALL(sender, sink(AsString("1d4#6E6E0000074621C0")));
+//      poll_callback->invoke();
+//
+//      EXPECT_CALL(sender, sink(AsString("11a#4E4004AAC00000BD")));
+//      EXPECT_CALL(sender, sink(AsString("1f2#106400B4001E0184")));
+//      EXPECT_CALL(sender, sink(AsString("1d4#6E6E000047462107")));
+//      poll_callback->invoke();
+//
+//      EXPECT_CALL(sender, sink(AsString("11a#4E400455000001AB")));
+//      EXPECT_CALL(sender, sink(AsString("1f2#106400B4001E0285")));
+//      EXPECT_CALL(sender, sink(AsString("1d4#6E6E0000874621CB")));
+//      poll_callback->invoke();
+//
+//      EXPECT_CALL(sender, sink(AsString("11a#4E40045540000266")));
+//      EXPECT_CALL(sender, sink(AsString("1f2#106400B4001E0386")));
+//      EXPECT_CALL(sender, sink(AsString("1d4#6E6E0000C746210C")));
+//      poll_callback->invoke();
+//   }
+//}
 
 TEST_F(HappyPollerTest, send0xFFResponseToInvalid55B)
 {
-   EXPECT_CALL(sender, sink(AsString("50c#00000000FFFF")));
+//   EXPECT_CALL(sender, sink(AsString("50c#00000000FFFF")));
    EXPECT_CALL(sender, sink(AsString("50b#0000000000000100")));
 
    poller.received(StandardDataFrame("55b#0000000000000000"));
@@ -111,42 +111,42 @@ TEST_F(HappyPollerTest, send0xFFResponseToInvalid55B)
 TEST_F(HappyPollerTest, respondTo55B)
 {
    // count = 0
-   EXPECT_CALL(sender, sink(AsString("50c#000000005DC8")));
+//   EXPECT_CALL(sender, sink(AsString("50c#000000005DC8")));
    EXPECT_CALL(sender, sink(AsString("50b#0000000000000100")));
    poller.received(StandardDataFrame("55b#0000AA0000000000"));
 
    // count = 1
-   EXPECT_CALL(sender, sink(AsString("50c#00000001B231")));
+//   EXPECT_CALL(sender, sink(AsString("50c#00000001B231")));
    EXPECT_CALL(sender, sink(AsString("50b#0000000000000100")));
    poller.received(StandardDataFrame("55b#0000550000000000"));
 
    // count = 2
-   EXPECT_CALL(sender, sink(AsString("50c#000000025D63")));
+//   EXPECT_CALL(sender, sink(AsString("50c#000000025D63")));
    EXPECT_CALL(sender, sink(AsString("50b#0000000000000100")));
    poller.received(StandardDataFrame("55b#0000AA0000000000"));
 
    // count = 3
-   EXPECT_CALL(sender, sink(AsString("50c#00000003B29A")));
+//   EXPECT_CALL(sender, sink(AsString("50c#00000003B29A")));
    EXPECT_CALL(sender, sink(AsString("50b#0000000000000100")));
    poller.received(StandardDataFrame("55b#0000550000000000"));
 
    // count = 0
-    EXPECT_CALL(sender, sink(AsString("50c#00000000B2A6")));
+//    EXPECT_CALL(sender, sink(AsString("50c#00000000B2A6")));
     EXPECT_CALL(sender, sink(AsString("50b#0000000000000100")));
     poller.received(StandardDataFrame("55b#0000550000000000"));
 
     // count = 1
-    EXPECT_CALL(sender, sink(AsString("50c#000000015D5F")));
+//    EXPECT_CALL(sender, sink(AsString("50c#000000015D5F")));
     EXPECT_CALL(sender, sink(AsString("50b#0000000000000100")));
     poller.received(StandardDataFrame("55b#0000AA0000000000"));
 
     // count = 2
-    EXPECT_CALL(sender, sink(AsString("50c#00000002B20D")));
+//    EXPECT_CALL(sender, sink(AsString("50c#00000002B20D")));
     EXPECT_CALL(sender, sink(AsString("50b#0000000000000100")));
     poller.received(StandardDataFrame("55b#0000550000000000"));
 
     // count = 3
-    EXPECT_CALL(sender, sink(AsString("50c#000000035DF4")));
+//    EXPECT_CALL(sender, sink(AsString("50c#000000035DF4")));
     EXPECT_CALL(sender, sink(AsString("50b#0000000000000100")));
     poller.received(StandardDataFrame("55b#0000AA0000000000"));
 }
