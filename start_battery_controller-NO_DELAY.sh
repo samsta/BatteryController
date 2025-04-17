506d3f9138b6e0b5e72ac9df0b434eb14638b8c9
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2025-04-18 09:09:25 +1200

    Create start_battery_controller-NO_DELAY.sh

---
+++ start_battery_controller-NO_DELAY.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /
+cd home/pi/Documents/Projects/BatteryController/build/
+/usr/bin/x-terminal-emulator -e ./testcan 4 can0 /dev/ttyTSY11576900 /dev/ttyTSY12508710
