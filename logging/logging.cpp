1fc9d38c74496941a32094e97d3df46d8d9491f2
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2025-11-13 10:24:29 +1300

    enable data logging to BatteryHikotron

--- logging/logging.cpp
+++ logging/logging.cpp
@@ -33,8 +33,8 @@ using namespace logging;
 // Log file name. File name should be change from here only
 const string logFileName = "BatteryController.log";
 const string dataFileName= "BatteryOneDataLog.txt";
-// const string httpPostURL = "http://jimster.ca/BatteryOne/TEST-data-receiver.php";
-const string httpPostURL = "http://jimster.ca/BatteryOne/BatteryOne-data-receiver.php";
+const string httpPostURL = "http://jimster.ca/BatteryOne/TEST-data-receiver.php";
+// const string httpPostURL = "http://jimster.ca/BatteryHikotron/BatteryOne-data-receiver.php";
 
 Logger::Logger(LOG_LEVEL loglevel, core::Timer& timer, std::vector<monitor::Monitor*> vmonitor):
    m_timer(timer),
@@ -65,13 +65,13 @@ Logger::Logger(LOG_LEVEL loglevel, core::Timer& timer, std::vector<monitor::Moni
    // }
 
    // set callback to 5 secs in order to sync up to the start of a minute
-   // m_timer.registerPeriodicCallback(&m_datalog_callback, 5000, "LogBatteryData5sec");
+   m_timer.registerPeriodicCallback(&m_datalog_callback, 5000, "LogBatteryData5sec");
 }
 
 Logger::~Logger()
 {
    m_File.close();
-   // m_timer.deregisterCallback(&m_datalog_callback);
+   m_timer.deregisterCallback(&m_datalog_callback);
 
    // pthread_mutexattr_destroy(&m_Attr);
    // pthread_mutex_destroy(&m_Mutex);
