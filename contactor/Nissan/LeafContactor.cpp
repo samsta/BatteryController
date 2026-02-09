58c810b8417b73b647294dcd898d54b2f5519129
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2026-02-10 11:58:56 +1300

    update messages

--- contactor/Nissan/LeafContactor.cpp
+++ contactor/Nissan/LeafContactor.cpp
@@ -90,7 +90,7 @@ void LeafContactor::openBothRelays()
    if (m_log)
    {
       std::ostringstream ss;
-      ss <<  "LeafContactor: contactor opened";
+      ss <<  "LeafContactor: all contactors open";
       m_log->info(ss);
    }
    m_state = OPEN;
