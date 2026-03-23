fd575ad371f7c9351707109ff97bcbf85197fe5a
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2026-03-23 16:25:24 +1300

    rename old autostart stuff

--- AutoStartupBC/start_battery_controller.sh
+++ AutoStartupBC/start_battery_controller.sh
@@ -3,7 +3,7 @@
 
 cd /home/pi/Documents/Projects/BatteryController/build/ || exit 1
 
-echo 'sleep 30, then start battery controller'
+echo 'sleep 30, then start battery controller Niall Deye'
 sleep 30
 
 /usr/bin/x-terminal-emulator -e ./testcan 4 candummy /dev/ttyACM0 usbdummy
