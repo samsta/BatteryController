79a39448b81e23c1f1f4b8d1a534af834d3367ec
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2026-03-30 13:56:31 +1300

    reduce max to 85 percent

--- packs/Nissan/LeafMultiPack.cpp
+++ packs/Nissan/LeafMultiPack.cpp
@@ -195,7 +195,7 @@ void LeafMultiPack::updateFullyChargedDischargedStatus()
 {
    bool use0to100limites =  false;
    float batcaplow = 5.0;
-   float batcaphigh = 90.0;
+   float batcaphigh = 85.0;
    float hysteresis = 0;
    
    // check/set fully charged/discharged status with hysteresis
