93a4ace3bb43e47b94fbc7fc3535fd8a136f4384
JimsterCoder <51102348+JimsterCoder@users.noreply.github.com>
2026-04-08 10:46:33 +1200

    fix indentation

--- core/USBInterface/USBInterface.cpp
+++ core/USBInterface/USBInterface.cpp
@@ -219,34 +219,33 @@ void USBPort::handle()
          {
             // error, discard data
             // display bad msg 
-            // if (findhash < (sizeof(cbuf)-100))
-               char errbuf[1024];
-               char errbuf2[1024];
-               char hexbuf[1024]; 
-               char charbuf[1024];
-               int hexlen = 0;
-               int charlen = 0;
+            char errbuf[1024];
+            char errbuf2[1024];
+            char hexbuf[1024]; 
+            char charbuf[1024];
+            int hexlen = 0;
+            int charlen = 0;
 
-               for (int i = 0; i < m_unprocessedSize && hexlen < (int)sizeof(hexbuf) - 3; ++i)
-               {
-                  hexlen += snprintf(hexbuf + hexlen, sizeof(hexbuf) - hexlen, "%02X ", (unsigned char)m_inBufferUnprocessed[i]);
-                  charlen += snprintf(charbuf + charlen, sizeof(charbuf) - charlen, "%c", isprint(m_inBufferUnprocessed[i]) ? m_inBufferUnprocessed[i] : '.');
-               }
-               snprintf(errbuf,sizeof(errbuf), "%s: Receive ERROR: bad msg format: len= %d  %.*s", m_port_name.c_str(), m_unprocessedSize, 3 *m_unprocessedSize, hexbuf);
-               snprintf(errbuf2,sizeof(errbuf2), "%.*s", m_unprocessedSize, charbuf);
-               // check for boot messages from ESP32
-               if (charbuf[0] == ':' || charbuf[0] == 'D' || charbuf[0] == 'd' || charbuf[0] == 'e' || charbuf[0] == 'S')
-               {
-                  // these are boot messages from ESP32
-                  if (m_log) m_log->info(errbuf2, __FILENAME__,__LINE__);
-                  if (m_log) m_log->info(errbuf);
-               }
-               else {
-                  // unknown messages
-                  if (m_log) m_log->error(errbuf2, __FILENAME__,__LINE__);
-                  if (m_log) m_log->error(errbuf);
-               }
-               m_unprocessedSize = 0;
+            for (int i = 0; i < m_unprocessedSize && hexlen < (int)sizeof(hexbuf) - 3; ++i)
+            {
+               hexlen += snprintf(hexbuf + hexlen, sizeof(hexbuf) - hexlen, "%02X ", (unsigned char)m_inBufferUnprocessed[i]);
+               charlen += snprintf(charbuf + charlen, sizeof(charbuf) - charlen, "%c", isprint(m_inBufferUnprocessed[i]) ? m_inBufferUnprocessed[i] : '.');
+            }
+            snprintf(errbuf,sizeof(errbuf), "%s: Receive ERROR: bad msg format: len= %d  %.*s", m_port_name.c_str(), m_unprocessedSize, 3 *m_unprocessedSize, hexbuf);
+            snprintf(errbuf2,sizeof(errbuf2), "%.*s", m_unprocessedSize, charbuf);
+            // check for boot messages from ESP32
+            if (charbuf[0] == ':' || charbuf[0] == 'D' || charbuf[0] == 'd' || charbuf[0] == 'e' || charbuf[0] == 'S')
+            {
+               // these are boot messages from ESP32
+               if (m_log) m_log->info(errbuf2, __FILENAME__,__LINE__);
+               if (m_log) m_log->info(errbuf);
+            }
+            else {
+               // unknown messages
+               if (m_log) m_log->error(errbuf2, __FILENAME__,__LINE__);
+               if (m_log) m_log->error(errbuf);
+            }
+            m_unprocessedSize = 0;
          }
       }
       // adjust the data in inbuf
